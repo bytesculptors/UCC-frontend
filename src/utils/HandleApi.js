f01d3ccabb8d5dddffa9986181234d85c2b25424
Tuan Nghia <tuannghia20062003@gmmail.com>
2023-07-03 17:58:04 +0700

    Add client

---
+++ src/utils/HandleApi.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+
+const baseUrl = "http://localhost:5001"
+
+const getAllTodo = (setToDo) => {
+    axios.get(baseUrl)
+        .then(({ data }) => {
+            console.log('data --->', data)
+            setToDo(data)
+        })
+        .catch((err) => { console.log(err) })
+}
+
+const addToDo = (text, setText, setToDo) => {
+    axios.post(`${baseUrl}/save`, { text })
+        .then((data) => {
+            console.log(data);
+            setText("");
+            getAllTodo(setToDo);
+        })
+        .catch((err) => { console.log(err) })
+}
+
+const updateToDo = (toDoId, text, setToDo, setText, setIsUpdating) => {
+    axios.post(`${baseUrl}/update`, { _id: toDoId, text })
+        .then((data) => {
+            console.log(data);
+            setText("");
+            setIsUpdating(false)
+            getAllTodo(setToDo);
+        })
+        .catch((err) => { console.log(err) })
+}
+
+const deleteToDo = (_id, setToDo) => {
+    axios.post(`${baseUrl}/delete`, { _id })
+        .then((data) => {
+            console.log(data);
+            getAllTodo(setToDo);
+        })
+        .catch((err) => { console.log(err) })
+}
+
+
+export { getAllTodo, addToDo, updateToDo, deleteToDo }
